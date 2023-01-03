0796ed89cb151e7bf31bc6662b98ee473c9c16c1
Negaar <ngr.rzvnfr@gmail.com>
2023-01-03 21:25:39 +0330

    init sql_app\model.py

---
+++ sql_app/model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from .database import Base
+from sqlalchemy.types import Text
+
+class User(Base):
+    __tablename__ = "user_info"
+
+    user_name  = Column(String)
+    student_num = Column(Integer, primary_key=True)
+    user_email = Column(String)
+    phone_num = Column(Integer)
+    user_password = Column(String)
+    user_role = Column(String)
+
+class Comment(Base):
+    __tablename__ = "comment_info"
+
+    hashtags = Column(Integer)
+    message = Column(Text)
+    comment_id = Column(Integer, primary_key=True)
